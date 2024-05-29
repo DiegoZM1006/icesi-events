98b5cadb3763aa5d65f93c165fa4aaae29b31bc2
DiegoZM1006 <1109184718@u.icesi.edu.co>
2024-05-28 20:52:19 -0500

    feat(Ciudades): Add getCity by id

--- src/main/java/icesi/edu/co/events/postgresDB/domain/Controller/CiudadesController.java
+++ src/main/java/icesi/edu/co/events/postgresDB/domain/Controller/CiudadesController.java
@@ -8,12 +8,10 @@ import lombok.RequiredArgsConstructor;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.CrossOrigin;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
 import java.util.List;
+import java.util.Optional;
 
 @RestController
 @RequestMapping("ciudades")
@@ -28,5 +26,11 @@ public class CiudadesController {
     public ResponseEntity<?> getCiudades(){
         List<Ciudades> ciudades = ciudadesRepository.findAll();
         return  new ResponseEntity<>(ciudades, HttpStatus.OK);
+    }
+
+    @GetMapping(value = "getCiudad/{id}")
+    public ResponseEntity<?> getCiudad(@PathVariable Integer id){
+        Optional<Ciudades> ciudad = ciudadesRepository.findById(id);
+        return  new ResponseEntity<>(ciudad, HttpStatus.OK);
     }
 }
